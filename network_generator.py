79e0bbdc14beafa73cf45b512f8077a2b434aa77
Rinaldo10 <rinaldojunior2012@gmail.com>
2019-11-11 14:47:48 -0400

    network_generator.py parseando nomes

--- network_generator.py
+++ network_generator.py
@@ -12,9 +12,25 @@ arquivo = json.loads(open("dados.json", "r").read())
 
 
 for cantor in arquivo:
+    lista = []
     for item in cantor:
         aux = (cantor[item][1])
         aux = (aux["Alb"])
         for album in aux[1:]:
             #ja consigo imprimir o nome dos albuns
-            
+            track = (album["tracks"])
+            track = track["items"]
+            for item in track:
+                artists = (item["artists"])
+                for name in artists:
+                    lista.append(name["name"])
+
+
+lista2 = []
+for i in lista:
+    if i not in lista2:
+        lista2.append(i)
+lista2.sort()
+
+for i in lista2:
+    print(i)
